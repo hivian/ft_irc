e6f01de92af8f9224895544b537d7e93d3afbfa6
Hugues IVIAN <hivian@e2r6p11.42.fr>
2017-02-23 12:04:29 +0100

    up

--- incs/ft_irc.h
+++ incs/ft_irc.h
@@ -6,13 +6,52 @@
 /*   By: hivian <hivian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/23 10:29:21 by hivian            #+#    #+#             */
-/*   Updated: 2017/02/23 10:29:46 by hivian           ###   ########.fr       */
+/*   Updated: 2017/02/23 12:04:07 by hivian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_IRC_H
 # define FT_IRC_H
 
+# include "../libft/incs/libft.h"
 # include <stdio.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/select.h>
+# include <sys/time.h>
+# include <stdbool.h>
+
+# define BUF_SIZE	4096
+# define FD_FREE	0
+# define FD_SERV	1
+# define FD_CLIENT	2
+# define MAX(a,b)	((a > b) ? a : b)
+
+typedef struct	s_fd
+{
+	int			type;
+ 	void		(*fct_read)();
+	void		(*fct_write)();
+ 	char		buf_read[BUF_SIZE + 1];
+	char		buf_write[BUF_SIZE + 1];
+}				t_fd;
+
+typedef struct		s_env
+{
+	t_fd			*fds;
+	int				port;
+	int				sock;
+	int				maxfd;
+	int				max;
+	int				r;
+	fd_set			fd_read;
+	fd_set			fd_write;
+}					t_env;
 
 #endif
--- srcs/server/main.c
+++ srcs/server/main.c
@@ -6,14 +6,70 @@
 /*   By: hivian <hivian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/23 10:30:39 by hivian            #+#    #+#             */
-/*   Updated: 2017/02/23 10:30:46 by hivian           ###   ########.fr       */
+/*   Updated: 2017/02/23 11:58:46 by hivian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_irc.h"
 
-int			main(int ac, char **av)
+static void				init_fd(t_env *e)
 {
-	printf("SERVER\n");
+	int	i;
+
+	i = 0;
+	e->max = 0;
+	FD_ZERO(&e->fd_read);
+	FD_ZERO(&e->fd_write);
+	while (i < e->maxfd)
+	{
+		if (e->fds[i].type != FD_FREE)
+		{
+			FD_SET(i, &e->fd_read);
+			if (strlen(e->fds[i].buf_write) > 0)
+				FD_SET(i, &e->fd_write);
+			e->max = MAX(e->max, i);
+		}
+		i++;
+	}
+}
+
+static int				create_server(int port)
+{
+	int					sock;
+	struct protoent		*proto;
+	struct sockaddr_in	sin;
+
+	proto = getprotobyname("tcp");
+	if (!proto)
+		return (-1);
+	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
+	{
+		ft_putendl("Bind error");
+		exit(-1);
+	}
+	listen(sock, 42);
+	return (sock);
+}
+
+int				main(int ac, char **av)
+{
+	t_env		e;
+	int			port;
+	int			sock;
+
+	if (ac != 2)
+	{
+		ft_putendl("Usage : ./serveur <port>");
+		exit(EXIT_FAILURE);
+	}
+	port = atoi(av[1]);
+	//printf("%d\n", port);
+	sock = create_server(port);
+	init_fd(&e);
+	close(sock);
 	return (0);
 }
